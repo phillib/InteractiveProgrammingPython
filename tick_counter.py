59caa1fdfdb96b5564e0c13107b022e0c5b9f6b6
phillib <phillib@users.noreply.github.com>
2015-03-07 20:15:31 -0600

    Create tick_counter.py

---
+++ tick_counter.py
@@ -0,0 +1,38 @@
+# SimpleGUI program template
+
+# Import the module
+import simplegui
+
+# Define global variables (program state)
+
+counter = 0
+
+# Define "helper" functions
+
+def increment():
+    global counter
+    counter = counter + 1
+
+# Define event handler functions
+
+def tick():
+    increment()
+    print counter
+    
+def buttonpress(): # when button is pressed counter resets to zero.
+    global counter
+    counter = 0 
+
+# Create a frame
+
+frame = simplegui.create_frame("SimpleGUI Test", 100, 100)
+
+# Register event handlers
+
+timer = simplegui.create_timer(1000, tick)
+frame.add_button("Click me!", buttonpress)
+
+# Start frame and timers
+
+frame.start()
+timer.start()
